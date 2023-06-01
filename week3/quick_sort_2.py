65ef74d66afeecd1ed5e9b9ffb98362f1d7c7569
Chung Chris <chrischung>
2023-06-01 23:53:08 +0800

    Upload week2 tasks

---
+++ week3/quick_sort_2.py
@@ -0,0 +1,41 @@
+def partition(arr, low, high):
+    arr[low], arr[high] = arr[high], arr[low]
+    pivot = arr[low]
+
+    i, j = low+1, low+1
+    while j <= high:
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+        j += 1
+            
+    arr[i-1], arr[low] = arr[low], arr[i-1]
+    return i-1, high-low
+
+
+def quickSort(arr: list, low, high):
+    '''
+    Implement in-place solution
+    '''
+    cnt = 0
+    if low < high:
+        pivot, _cnt = partition(arr, low, high)
+        _cnt_left = quickSort(arr, low, pivot-1)
+        _cnt_right = quickSort(arr, pivot+1, high)
+        cnt += _cnt + _cnt_left + _cnt_right
+    
+    return cnt
+
+
+test_cases = [[8,3,4,5,9,2,4,6], [1,2,3,5,6,9], [2,1], [9,6,5,2,1]]
+for t in test_cases:
+    _cnt = quickSort(t, 0, len(t)-1)
+    print(t, _cnt)
+
+f = open("test_case.txt", "r")
+lines = [int(x.strip()) for x in f.readlines()]
+f.close()
+
+cnt = quickSort(lines, 0, len(lines)-1)
+print("Answer is: ", cnt)
+# 164123
---
+++ week3/quick_sort_3.py
@@ -0,0 +1,64 @@
+def partition(arr, low, high):
+    mid = low + (high-low)//2#(low+high)//2
+    a, b, c = arr[low], arr[mid], arr[high]
+    if a < b:
+        if b <= c:
+            # a < b <= c
+            pivot = mid
+        elif c >= a:
+            # a <= c < b
+            pivot = high
+        else:
+            # c < a < b
+            pivot = low
+    else:
+        # a >= b
+        if a < c:
+            # b <= a < c
+            pivot = low
+        elif b < c:
+            # b <= c <= a
+            pivot = high
+        else:
+            pivot = mid
+
+    arr[low], arr[pivot] = arr[pivot], arr[low]
+    pivot = arr[low]
+
+    i, j = low+1, low+1
+    while j <= high:
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+        j += 1
+            
+    arr[i-1], arr[low] = arr[low], arr[i-1]
+    return i-1, high-low
+
+
+def quickSort(arr: list, low, high):
+    '''
+    Implement in-place solution
+    '''
+    cnt = 0
+    if low < high:
+        pivot, _cnt = partition(arr, low, high)
+        _cnt_left = quickSort(arr, low, pivot-1)
+        _cnt_right = quickSort(arr, pivot+1, high)
+        cnt += _cnt + _cnt_left + _cnt_right
+    
+    return cnt
+
+
+test_cases = [[8,3,4,5,9,2,4,6], [1,2,3,5,6,9], [2,1], [9,6,5,2,1]]
+for t in test_cases:
+    _cnt = quickSort(t, 0, len(t)-1)
+    print(t, _cnt)
+
+f = open("test_case.txt", "r")
+lines = [int(x.strip()) for x in f.readlines()]
+f.close()
+
+cnt = quickSort(lines, 0, len(lines)-1)
+print("Answer is: ", cnt)
+# 138382
